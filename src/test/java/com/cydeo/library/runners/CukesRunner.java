f7bedda61f3ad249271d1521329d2ab2b91e5eb0
simil <similanl@hotmail.com>
2022-07-13 01:04:10 -0400

    git conflict

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -10,11 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/library/step_definitions",
         dryRun = false,
-<<<<<<< HEAD
         tags = "@testingExcel",
-=======
-        tags = "@testingExcel",
->>>>>>> develop
         publish = true
 )
 public class CukesRunner {
